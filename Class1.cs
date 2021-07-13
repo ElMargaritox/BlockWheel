7ff4dd663522efb6efbff0e035c96796f2efae1c
ElMargaritox <alexisguzman0303@gmail.com>
2021-07-13 15:30:41 -0300

    13/7/2021

---
+++ Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Rocket.API;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using Steamworks;
+using Logger = Rocket.Core.Logging.Logger;
+
+
+namespace EgnBlockWheel
+{
+    public class Class1 :  RocketPlugin<Config>
+    {
+        public Class1 Instance;
+        protected override void Load()
+        {
+            base.Load();
+            Instance = this;
+            Logger.Log("-=Plugin By: Margarita#8172", ConsoleColor.Yellow);
+
+            VehicleManager.onDamageTireRequested += DestroyWheel;
+        }
+
+        private void DestroyWheel(CSteamID instigatorSteamID, InteractableVehicle vehicle, int tireIndex, ref bool shouldAllow, EDamageOrigin damageOrigin)
+        {
+            UnturnedPlayer player = UnturnedPlayer.FromCSteamID(instigatorSteamID);
+
+            if (damageOrigin == EDamageOrigin.Useable_Gun & Configuration.Instance.Armas_Pueden_Destruir_Ruedas || damageOrigin == EDamageOrigin.Useable_Melee & Configuration.Instance.Melee_Pueden_Destruir_Ruedas)
+            {
+                if (player.HasPermission(Configuration.Instance.Permiso))
+                {
+                    shouldAllow = true;
+                }
+                else
+                {
+                    shouldAllow = false;
+                    return;
+                }
+            }
+            else
+            {
+                shouldAllow = false;
+                return;
+            }
+
+        }
+
+        protected override void Unload()
+        {
+            Instance.UnloadPlugin();
+
+        }
+    }
+}
---
+++ Config.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rocket.API;
+
+namespace EgnBlockWheel
+{
+    public class Config : IRocketPluginConfiguration
+    {
+        public string Permiso;
+        public bool Armas_Pueden_Destruir_Ruedas;
+        public bool Melee_Pueden_Destruir_Ruedas;
+        public void LoadDefaults()
+        {
+            Permiso = "egn.wheel";
+            Armas_Pueden_Destruir_Ruedas = true;
+            Melee_Pueden_Destruir_Ruedas = true;
+        }
+    }
+}
